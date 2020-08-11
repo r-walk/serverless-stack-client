c215980f2f257855b6d72f7ea158e24b4ec0054b
Rick Walker <r.walk@outlook.com>
2020-08-11 09:07:36 -0400

    Setting up React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-west-2",
+      BUCKET: "rw-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-west-2",
+      URL: "https://datqz9ubyc.execute-api.us-west-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-west-2",
+      USER_POOL_ID: "us-west-2_wOEfPXcjK",
+      APP_CLIENT_ID: "4vschtgou8r28doelvfe9al78",
+      IDENTITY_POOL_ID: "notes identity pool"
+    }
+  };
